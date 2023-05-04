b5680d38aff4cbf53f52278a25069b88f8fe31b5
Hanefi Gülbahar <hanefigulbahar@gmail.com>
2023-05-04 21:48:00 +0300

    Update error view

--- app/movie/error.tsx
+++ app/movie/error.tsx
@@ -3,7 +3,7 @@ import ErrorView from "@/components/error-view";
 import React from "react";
 
 function Movie404() {
-  return <ErrorView />;
+  return <ErrorView page="/" />;
 }
 
 export default Movie404;
